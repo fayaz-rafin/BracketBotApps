f1193c780586a63b77993bb3ee9441b379dc525f
Brian M <bmachado@live.ca>
2025-08-25 23:51:15 +0000

    fix speeds

--- wasd.py
+++ wasd.py
@@ -11,8 +11,8 @@ from sshkeyboard import listen_keyboard, stop_listening
 import numpy as np
 
 # Configuration
-TURN_SPEED = 2.0  # Linear speed in m/s
-SPEED = 0.5  # Angular speed in rad/s
+TURN_SPEED = 6.0  # Angular speed in m/s
+SPEED = 0.75  # Linear speed in rad/s
 # Global variables
 writer = None
 
